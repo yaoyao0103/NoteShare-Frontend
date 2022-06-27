c1d4ad3b4c9d81d82f876147191717ba579ac025
Yao <elites96105@gmail.com>
2022-06-28 00:10:04 +0800

    feat: verification page Modif

--- src/pages/OuterPage/OuterPage.js
+++ src/pages/OuterPage/OuterPage.js
@@ -52,6 +52,7 @@ const OuterPage = () => {
             case 'PersonalPage': setPageComponent(<PersonalPage page='PersonalPage' setPageProps={setPageProps} setCurrPage={setCurrPage} />); break;
             case 'LoginPage': setPageComponent(<LoginPage page='LoginPage' setPageProps={setPageProps} setCurrPage={setCurrPage} />); break;
             case 'SignUpPage': setPageComponent(<SignUpPage page='SignUpPage' setPageProps={setPageProps} setCurrPage={setCurrPage} />); break;
+            case 'VerificationPage': setPageComponent(<VerificationPage page='VerificationPage' setPageProps={setPageProps} setCurrPage={setCurrPage} {...pageProps}/>); break;
             default: setPageComponent(<></>); break;
         }
     },[currPage])
@@ -192,6 +193,14 @@ const OuterPage = () => {
                     setCurrPage('SignUpPage');
                 }}>
                     SignUpPage
+                </Button>
+                <Button type="primary" onClick={() =>{
+                    setPageProps({
+                        email: 'testemail@email.ntou.edu.tw'
+                    })
+                    setCurrPage('VerificationPage');
+                }}>
+                    VerificationPage
                 </Button>
             </Drawer>
         </>
