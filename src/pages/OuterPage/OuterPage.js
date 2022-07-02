0148dcbefbb7cf3f90908a7ba0a069936ed26320
Plusx <a147896325811@gmail.com>
2022-07-02 13:06:10 +0800

    merge

--- src/pages/OuterPage/OuterPage.js
+++ src/pages/OuterPage/OuterPage.js
@@ -63,12 +63,8 @@ const OuterPage = () => {
 
     useEffect(() => {
         console.log("page", pageProps.page)
-<<<<<<< HEAD
-        console.log('login',loggedIn)
-=======
         console.log("pageProps", pageProps)
         
->>>>>>> 38e728f386a193baa0cf2129a0a2c4976b8b6f94
         switch (pageProps.page) {
             case 'NoteDetailPage': setPageComponent(<NoteDetailPage page='NoteDetailPage' setPageProps={setPageProps} {...pageProps} />); break;
             case 'NoteEditPage': setPageComponent(<NoteEditPage page='NoteEditPage' setPageProps={setPageProps}  {...pageProps} />); break;
